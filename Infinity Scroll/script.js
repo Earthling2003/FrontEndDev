2a588faa65c2889be6dff654daa8bfcee92178f0
Earthling2003 <curious.introverted@gmail.com>
2022-07-27 15:14:45 +0800

    creates displayPhotos function: for each photos add an a element and set an attribute href,target, attribute. creates also an image element and set attributes such as title, alt and src. put img element in <a> element and out the <a> element in image-container.

--- Infinity Scroll/script.js
+++ Infinity Scroll/script.js
@@ -8,16 +8,63 @@ const count = 10;
 const apiKey ='CSFRBsPJSODklH9KFjfSsksT9ZeIenE6KXs8dBCetwo';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
+// create Elements for links & Photos, Add to DOM
+function displayPhotos(){
+    // Run function for each object in photosArray
+    photosArray.forEach((photo) => {
+        // create <a> to link to Unsplash
+        const item = document.createElement('a');
+        item.setAttribute('href', photo.links.html);
+        item.setAttribute('target', '_blank');
+
+        // create <img> for photo
+        const img =document.createElement('img');
+        img.setAttribute('src', photo.urls.regular);
+        img.setAttribute('alt', photo.alt_description);
+        img.setAttribute('title', photo.alt_description);
+        // put <img? inside <a>, then put both
+        item.appendChild(img);
+        imageContainer.appendChild(item);
+
+
+    });
+ }
+
+ // create Elements for links & Photos, Add to DOM
+function displayPhotos(){
+    // Run function for each object in photosArray
+    photosArray.forEach((photo) => {
+        // create <a> to link to Unsplash
+        const item = document.createElement('a');
+        item.setAttribute('href', photo.links.html);
+        item.setAttribute('target', '_blank');
+
+        // create <img> for photo
+        const img =document.createElement('img');
+        img.setAttribute('src', photo.urls.regular);
+        img.setAttribute('alt', photo.alt_description);
+        img.setAttribute('title', photo.alt_description);
+        // put <img? inside <a>, then put both
+        item.appendChild(img);
+        imageContainer.appendChild(item);
+
+
+    });
+ }
+
 // Get Photos from unsplash API
 
 async function getPhotos(){
     try{
         const response = await fetch(apiUrl);
         photosArray = await response.json();
-        
+        console.log(photosArray);
+        displayPhotos();
     }catch (error) {
         // 
     }
 }
+
+
 
 getPhotos()
