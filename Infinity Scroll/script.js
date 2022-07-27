5a8b7809217fc92946e13764c60bbe59cc718591
Earthling2003 <curious.introverted@gmail.com>
2022-07-27 15:34:08 +0800

    Optimizing code: avoiding dry code ,do not repeat yourself.

--- Infinity Scroll/script.js
+++ Infinity Scroll/script.js
@@ -8,20 +8,35 @@ const count = 10;
 const apiKey ='CSFRBsPJSODklH9KFjfSsksT9ZeIenE6KXs8dBCetwo';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
+function setAttributes(element, attributes) {
+    for (const key in attributes) {
+        element.setAttribute(key, attributes[key])
+    }
+}
+
 // create Elements for links & Photos, Add to DOM
 function displayPhotos(){
     // Run function for each object in photosArray
     photosArray.forEach((photo) => {
         // create <a> to link to Unsplash
         const item = document.createElement('a');
-        item.setAttribute('href', photo.links.html);
-        item.setAttribute('target', '_blank');
+        // item.setAttribute('href', photo.links.html);
+        // item.setAttribute('target', '_blank');
+        setAttributes(item, {
+            href: photo.links.html,
+            target: '_blank'
 
-        // create <img> for photo
+        });        // create <img> for photo
         const img =document.createElement('img');
-        img.setAttribute('src', photo.urls.regular);
-        img.setAttribute('alt', photo.alt_description);
-        img.setAttribute('title', photo.alt_description);
+        // img.setAttribute('src', photo.urls.regular);
+        // img.setAttribute('alt', photo.alt_description);
+        // img.setAttribute('title', photo.alt_description);
+
+        setAttributes(img, {
+            src: photo.urls.regular,
+            alt: photo.alt_description,
+            title: photo.alt_description
+        });
         // put <img? inside <a>, then put both
         item.appendChild(img);
         imageContainer.appendChild(item);
